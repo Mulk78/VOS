a41635291c374aa6ba947c5140479f4edd322717
Mulk78 <taalveiko@gmail.com>
2024-06-11 17:12:02 +0300

    esimene tund

---
+++ koduleht/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div classname="App">
+      <img classname="pilt" src="" alt="" />
+      <button classname="nupu-Stiil">Vajuta siia</button>
+      <button>Niisama </button>
+    </div>
+  );
+}
+
+export default App;
