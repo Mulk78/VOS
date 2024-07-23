3fb855fa85674ab8970d86a796bda4bfd22339e2
Mulk78 <taalveiko@gmail.com>
2024-07-23 11:28:39 +0300

    230724

--- koduleht/src/App.js
+++ koduleht/src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div classname="App">
-      <img classname="pilt" src="" alt="" />
-      <button classname="nupu-Stiil">Vajuta siia</button>
-      <button>Niisama </button>
+    <div>
+            
     </div>
   );
 }
